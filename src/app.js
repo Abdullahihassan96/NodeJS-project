e11207af9b34c8c3394ee4269d5fd7001a4848ac
Abdullahihassan96 <ibnuhashoy96@gmail.com>
2025-07-23 17:54:05 +0300

    exploring http methods

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const app = express();
 //   res.send("Hello, World!");
 // }); //this code listens to all requests and sends a response regardless of path (/, /about, /contact, etc.)
 
+//order of executation also matters in express.js, so if you put the above code before the routes, it will not reach the routes.
+
 //since the port listens all the requests, we add routes to specify requests
 
 app.get("/", (req, res) => {
@@ -22,6 +24,34 @@ app.get("/contact", (req, res) => {
 
 app.get("/products", (req, res) => {
   res.send("Here are our products!");
+});
+
+//since app.use(); matches all requests by default, we should use app.get(), app.post(), etc. for specific routes.
+
+//example GET, POST, PUT, PATCH AND DELETE using /user
+app.get("/user", (req, res) => {
+  // logic to fetch data from the db
+  res.send("Data fetched successfully!");
+});
+
+app.post("/user", (req, res) => {
+  // logic saving data to the db
+  res.send("Data saved successfully!");
+});
+
+app.put("/user", (req, res) => {
+  // logic to update data in the db
+  res.send("Data updated successfully!");
+});
+
+app.patch("/user", (req, res) => {
+  // logic to partially update data in the db
+  res.send("Data partially updated successfully!");
+});
+
+app.delete("/user", (req, res) => {
+  // logic to delete data from the db
+  res.send("Data deleted successfully!");
 });
 
 app.listen(5555, () => {
